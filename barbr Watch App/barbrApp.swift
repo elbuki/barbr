54b6a62881e585e7d3ebad1b4129dcfcdd9b4f77
Marco Carmona <elbuki@users.noreply.github.com>
2023-02-08 14:47:26 -0600

    Initial Commit

---
+++ barbr Watch App/barbrApp.swift
@@ -0,0 +1,17 @@
+//
+//  barbrApp.swift
+//  barbr Watch App
+//
+//  Created by Marco Carmona on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct barbr_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
